60639d57218634c687f3c580c008e9b65a977e68
EstherXu <96934319+estherxyx@users.noreply.github.com>
2022-01-10 15:09:59 -0800

    create table in ddb

--- aws-cdk/dev/lib/dev-stack.ts
+++ aws-cdk/dev/lib/dev-stack.ts
@@ -1,5 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cdk from '@aws-cdk/core';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DevStack extends Stack {
@@ -12,5 +14,16 @@ export class DevStack extends Stack {
     // const queue = new sqs.Queue(this, 'DevQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const table = new dynamodb.Table(this, id, {
+      tableName: "Movies",
+      billingMode: dynamodb.BillingMode.PROVISI ONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {name: 'year', type: dynamodb.AttributeType.NUMBER},
+      sortKey: {name: 'title', type: dynamodb.AttributeType.STRING},
+      pointInTimeRecovery: true,
+    });
   }
 }
